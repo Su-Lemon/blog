33d403aadd873c1af215e1a3f62d7075f7834bf0
Su-Lemon <suwei199909@163.com>
2021-02-22 17:42:40 +0800

    Site updated: 2021-02-22 17:42:39

--- js/search.js
+++ js/search.js
@@ -91,5 +91,5 @@ var searchFunc = function(path, search_id, content_id) {
     });
 };
 
-var path = "/search.xml";
+var path = "/blog/search.xml";
 searchFunc(path, 'local-search-input', 'local-search-result');
